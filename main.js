d927de947029ad47fceebb2733fda79d30602286
Alessandra Colvara de Souza <peglowalessandra@gmail.com>
2021-10-24 05:05:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const LinksSocialMedia = {
+    github: 'alepeglow',
+    youtube: 'channel/UCsW6amPQQJk18K-sj6cv15A',
+    facebook: 'alelovemusic',
+    instagram: 'alepeglow_',
+    twitter: 'litlebiblioteca'
+  }
+  
+  function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute('class')
+  
+      li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+  }
+  
+  changeSocialMediaLinks()
+  
+  function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        UserImage.src = data.avatar_url
+        userLogin.textContent = data.login
+      })
+  }
+  
+  getGitHubProfileInfos()
+
+
+
+
+
+
+
+
+
+
